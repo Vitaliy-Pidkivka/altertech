67945058b9a9872a3fcdb6cbccc5af1731095af6
vpidkivka <vpidkivka@eliteit.org>
2020-06-19 22:46:40 +0300

    Updates

---
+++ precache-manifest.7f7612750b0486fd36b8a4bb977c8316.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1ae2c698981441d84164da27890a82f",
+    "url": "/altertech/index.html"
+  },
+  {
+    "revision": "cc3a0ab4bb8727239ec7",
+    "url": "/altertech/static/css/main.f56a913d.chunk.css"
+  },
+  {
+    "revision": "eefaf15c6a95e740ae2b",
+    "url": "/altertech/static/js/2.865e5353.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/altertech/static/js/2.865e5353.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc3a0ab4bb8727239ec7",
+    "url": "/altertech/static/js/main.c4dc3ff9.chunk.js"
+  },
+  {
+    "revision": "b8607c0ab73580c9f943",
+    "url": "/altertech/static/js/runtime-main.dac34f9e.js"
+  }
+]);
